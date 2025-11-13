2cd3487d3cd2069867d2707b4915cf61cfa08685
TerryMinn <shinnthantmindev.mm@gmail.com>
2025-11-14 00:07:07 +0700

    feat: array and slice

--- main.go
+++ main.go
@@ -1,76 +1,49 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
+
 
 func main(){
-	// reader := bufio.NewReader(os.Stdin);
-	// input,_ := reader.ReadString('\n');
+	// Array တွေ က fixed တွေပါ
+	// nums := [4]int{1, 2, 3, 4}
+	// slice က flex ဖြစ်ပါတယ် Append တို့  Cap တို့ က Slice မှာရှိပါတယ်
+	nums := []int{2,3,4,5,6,6}
+	
+	strs := [3]string{"Terry","Pokemon","Leon"}
+	bools := [2]bool{true,false}
+	runes := [2]rune{'c','d'}
 
-	   // Compare
-    fmt.Println(strings.Compare("apple", "banana")) // -1 (apple < banana)
-    fmt.Println(strings.Compare("banana", "apple")) // 1  (banana > apple)
-    fmt.Println(strings.Compare("apple", "apple"))  // 0  (equal)
+	fmt.Println(nums)
+	fmt.Println(strs)
+	fmt.Println(bools)
+	fmt.Println(runes)
+	fmt.Printf("%c \n",runes)
+	// lens ဆိုတာ Array/Slice မှာ length ဘယ်လောက်ရှိလည်ဆိုကို ကြည့်တာပါ
+	fmt.Println("lens in array",len(nums),len(strs),len(bools),len(runes))
+	// caps ဆိုတာ ကတော့ array မှာ ဆို length ပါပဲ slice မှာ ဆို ရက် တော့ increse count ပါ ကြို defind လုပ်ထားလို့လည်းရပါတယ်
+	fmt.Println("caps in array",cap(nums),cap(strs),cap(bools),cap(runes))
 
-    // Contains
-    fmt.Println(strings.Contains("Hello World", "World")) // true
-    fmt.Println(strings.Contains("Hello World", "world")) // false (case-sensitive)
+	// Copy လုပ်တဲ့နည်းပါ
+	src := []int{1,2,3,4}
+	// Make ဆိုတာ slice ကို type ကြိုကြေညှားထားတဲ့ သဘောပါပဲ 
+	dis := make([]int,2)
 
-    // ContainsAny
-    fmt.Println(strings.ContainsAny("Hello", "xyz")) // false
-    fmt.Println(strings.ContainsAny("Hello", "aeiou")) // true (contains 'e' and 'o')
+	// copy ကတော့ slice မှာ copy ကူးတာ ပါ return value length ကိုရပါတယ် initialize လုပ်ပြီ သား dis ရယ် source
+	n := copy(dis,src);
+	fmt.Println(n,dis)
 
-    // ContainsRune
-    fmt.Println(strings.ContainsRune("Hello", 'H')) // true
-    fmt.Println(strings.ContainsRune("Hello", 'z')) // false
+	// slice မှာ ပဲသုံးလို့ရပါတယ် ထပ်ထည့်တာပါ
+	dis = append(dis,src...)
 
-    // HasPrefix
-    fmt.Println(strings.HasPrefix("Hello World", "He")) // true
-    fmt.Println(strings.HasPrefix("Hello World", "Wo")) // false
+	fmt.Println(dis[0])
 
-    // HasSuffix
-    fmt.Println(strings.HasSuffix("Hello World", "ld")) // true
-    fmt.Println(strings.HasSuffix("Hello World", "lo")) // false
 
-    // EqualFold (case-insensitive compare)
-    fmt.Println(strings.EqualFold("GoLang", "golang")) // true
-    fmt.Println(strings.EqualFold("GoLang", "Golang!")) // false
-
-    fmt.Println(strings.Count("banana", "a")) // 3
-    fmt.Println(strings.Index("chicken", "ken")) // 4
-
-    fmt.Println(strings.IndexAny("team", "aeiou")) // 1 ('e')
-    fmt.Println(strings.IndexRune("Hello", 'o')) // 4
-    fmt.Println(strings.LastIndex("go gopher go", "go")) // 10
-    fmt.Println(strings.Split("a,b,c", ",")) // [a b c]
-    fmt.Println(strings.SplitN("a,b,c", ",", 2)) // [a b,c]
-    fmt.Println(strings.Fields(" foo bar baz ")) // [foo bar baz]
-    f := func(r rune) bool { return r == ',' || r == ';' }
-    fmt.Println(strings.FieldsFunc("a,b;c", f)) // [a b c]
-    before, after, found := strings.Cut("foo:bar", ":")
-    fmt.Println(before, after, found) // foo bar true
-    fmt.Println(strings.Replace("oink oink oink", "oink", "moo", 2))// moo moo oink
-    fmt.Println(strings.ReplaceAll("oink oink", "oink", "moo"))// moo moo
-    mapping := func(r rune) rune {
-    if r == 'a' { return 'x' }
-    return r
-    }
-    fmt.Println(strings.Map(mapping, "banana")) // bxnxnx
-
-    fmt.Println(strings.Trim("¡¡¡Hello!!!", "!¡")) // Hello
-    fmt.Println(strings.TrimSpace("   Hello World   ")) // Hello World
-    fmt.Println(strings.TrimPrefix("HelloWorld", "Hello")) // World
-    fmt.Println(strings.TrimSuffix("HelloWorld", "World")) // Hello
-
-    s := []string{"foo", "bar", "baz"}
-    fmt.Println(strings.Join(s, ", ")) // foo, bar, baz
-    fmt.Println(strings.Repeat("na", 3)) // nanana
-
-    fmt.Println(strings.ToLower("GoLang")) // golang
-    fmt.Println(strings.ToUpper("GoLang")) // GOLANG
-    fmt.Println(strings.Title("go language")) // Go Language
-    c := strings.Clone("Hello")
-    fmt.Println(c == "Hello") // true
+	// array ကို copy method သုံးခြင်ရင် array ကို slice view ပြောင်းလို့ရပါတယ် arr1[:] ပါပဲ
+	// arr1 := [3]int{1, 2, 3}
+     // arr2 := [3]int{0, 0, 0}
+  
+     // n := copy(arr2[:], arr1[:]) // convert arrays to slices
+     // fmt.Println("Copied:", n)   // 3
+     // fmt.Println("arr2:", arr2)  // [1 2 3]
+	
 }
